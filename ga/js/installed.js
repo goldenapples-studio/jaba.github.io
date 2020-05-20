b5fbe46f327f31e1f99754c16be79a8699365700
goldenapples-studio <65478788+goldenapples-studio@users.noreply.github.com>
2020-05-20 08:51:48 +0800

    Add files via upload

---
+++ ga/js/installed.js
@@ -0,0 +1,1 @@
+function getJvav(){document.getElementById("tou_block").style.display="none";document.getElementById("verify_block").style.display="none";document.getElementById("jvav_block").style.display="";}
